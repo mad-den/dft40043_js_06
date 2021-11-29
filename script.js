4a1cf7dcf0e37231f1203b6a64f70e7edcad6de5
mad-den <nurahmadfi@gmail.com>
2021-11-29 15:01:17 +0800

    latihan

---
+++ script.js
@@ -0,0 +1,6 @@
+function fokus () {
+    document.getElementById('mesej').innerText = 'Sila klik butang Simpan';
+   }
+   function bler() {
+    document.getElementById('mesej').innerText = 'Sila nyatakan keperluan anda';
+   }
